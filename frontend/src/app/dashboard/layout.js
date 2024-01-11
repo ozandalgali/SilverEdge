0d85d9558faca44db83aaf1673f760cf61f29bc0
ozan <ozan.dalgali@gmail.com>
2024-01-12 01:12:57 +0300

    In progress

--- frontend/src/app/dashboard/layout.js
+++ frontend/src/app/dashboard/layout.js
@@ -1,17 +1,14 @@
-import Head from "next/head";
 import Sidebar from "../ui/dashboard/sidebar/sidebar";
 import Navbar from "../ui/dashboard/navbar/navbar";
+import styles from "../ui/dashboard/dashboard.module.css";
 
 const Layout = ({children}) => (
     (
-        <div>
-            <Head>
-                <title>SilverEdge</title>
-            </Head>
-            <div>
+        <div className={styles.container}>
+            <div className={styles.menu}>
                 <Sidebar />
             </div>
-            <div>
+            <div className={styles.content}>
                 <Navbar />
                 {children}
             </div>
--- frontend/src/app/ui/dashboard/sidebar/sidebar.js
+++ frontend/src/app/ui/dashboard/sidebar/sidebar.js
@@ -1,9 +1,100 @@
+import styles from "./sidebar.module.css";
+import Image from "next/image";
+import MenuLink from "./MenuLink/menulink";
+import { MdDashboard, MdShoppingCart, MdReceipt, MdPeople, MdLogout, MdSettings } from "react-icons/md";
+
+
+const menuItems = [
+    {
+        title:"CEX",
+        list: [{
+            title: "Dashboard",
+            path: "/dashboard",
+            icon: <MdDashboard />
+        },
+        {
+            title: "Products",
+            path: "/dashboard/products",
+            icon: <MdShoppingCart />
+        },
+        {
+            title: "Rotator",
+            path: "/dashboard/users",
+            icon: <MdReceipt />
+        },
+        {
+            title: "Customers",
+            path: "/dashboard/transactions",
+            icon: <MdPeople />
+        },
+        {
+            title: "Settings",
+            path: "/dashboard/settings",
+            icon: <MdSettings />
+        }]},
+        {
+
+        title:"Onchain Tools",
+        list: [{
+            title: "Wallet Tracker",
+            path: "/wallettracker",
+            icon: <MdDashboard />
+        },
+        {
+            title: "Products",
+            path: "/dashboard/products",
+            icon: <MdShoppingCart />
+        },
+        {
+            title: "Sniper",
+            path: "/dashboard/users",
+            icon: <MdReceipt />
+        },
+        {
+            title: "Customers",
+            path: "/dashboard/transactions",
+            icon: <MdPeople />
+        },
+        {
+            title: "Settings",
+            path: "/dashboard/settings",
+            icon: <MdSettings />
+        }],
+    },
+]
 function Sidebar() {
     return (
-        <div>
-            Sidebar
+        <div className={styles.container}>
+        <div className={styles.user}>
+          <Image
+            className={styles.userImage}
+            src="/noavatar.png"
+            alt=""
+            width="50"
+            height="50"
+          />
+          <div className={styles.userDetail}>
+            <span className={styles.username}>Mochhi</span>
+            <span className={styles.userTitle}>Administrator</span>
+          </div>
         </div>
+        <ul className={styles.list}>
+          {menuItems.map((cat) => (
+            <li key={cat.title}>
+              <span className={styles.cat}>{cat.title}</span>
+              {cat.list.map((item) => (
+                <MenuLink item={item} key={item.title} />
+              ))}
+            </li>
+          ))}
+        </ul><br></br>
+        <button className={styles.logout}>
+            <MdLogout/>
+            Logout</button>
+        </div>
+       
     )
-}
+};
+
 
 export default Sidebar;
